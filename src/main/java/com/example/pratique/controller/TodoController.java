1bf424c483d7c9462139f55f13270ce1bb395559
202510-aw-kudo <202510_aw_kudo@x-ide.sakura.ne.jp>
2026-02-05 10:21:55 +0900

    2-1 prompt (Controllerの基本)

--- src/main/java/com/example/pratique/controller/TodoController.java
+++ src/main/java/com/example/pratique/controller/TodoController.java
@@ -1,4 +1,4 @@
-package com.example.pratique.controller;
+﻿package com.example.pratique.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;

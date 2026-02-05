d2c7f9eb566ad0b06b9fd2185b27748be20e0b9d
202510-aw-kudo <202510_aw_kudo@x-ide.sakura.ne.jp>
2026-02-05 10:52:23 +0900

    @RequestParamでフォームデータ

--- src/main/java/com/example/pratique/controller/TodoController.java
+++ src/main/java/com/example/pratique/controller/TodoController.java
@@ -1,8 +1,11 @@
 ﻿package com.example.pratique.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TodoController {
@@ -17,6 +20,19 @@ public class TodoController {
   @GetMapping("/todos/new")
   public String newForm() {
     return "todos/new";
+  }
+
+  // ToDo入力内容の確認画面を表示する。
+  @PostMapping("/todos/confirm")
+  public String confirm(
+      @RequestParam("title") String title,
+      @RequestParam(value = "description", required = false) String description,
+      @RequestParam(value = "priority", defaultValue = "3") Integer priority,
+      Model model) {
+    model.addAttribute("title", title);
+    model.addAttribute("description", description);
+    model.addAttribute("priority", priority);
+    return "todos/confirm";
   }
 
   // 指定されたIDのToDo詳細画面を表示する。
